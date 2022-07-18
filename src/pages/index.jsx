037b8861f1bbb434fba6942f7d1d6873ee96d883
Nikita Pozdeev <6034438+nikitapozdeev@users.noreply.github.com>
2022-07-18 23:12:44 +0400

    add metadata

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,34 @@
+import Head from 'next/head';
+
+const meta = {
+  'description': 'Full stack web developer (JS/.NET)',
+  'image': 'https://www.npozdeev.com/preview.png',
+  'url': 'https://www.npozdeev.com',
+  'title': 'Nikita Pozdeev',
+}
+
 const Home = () => {
   return (
-    <article className="container__page">
-      <p><span className="greetings">Hi</span>, I am a full stack web developer (JS/.NET)</p>
-      <p>In my spare time I am interested in game development, open source, low-level programming</p>
-    </article>
+    <>
+      <Head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Nikita Pozdeev</title>
+        <meta name="description" content={meta.description}/>
+        <meta name="image" content={meta.image}/>
+        <meta name="og:url" content={meta.url} />
+        <meta name="og:title" content={meta.title} />
+        <meta name="og:description" content={meta.description} />
+        <meta name="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:title" content={meta.title}/>
+        <meta name="twitter:description" content={meta.description}/>
+        <meta name="twitter:image" content={meta.image}/>
+      </Head>
+      <article className="container__page">
+        <p><span className="greetings">Hi</span>, I am a full stack web developer (JS/.NET)</p>
+        <p>In my spare time I am interested in game development, open source, low-level programming</p>
+      </article>
+    </>
   )
 }
 
